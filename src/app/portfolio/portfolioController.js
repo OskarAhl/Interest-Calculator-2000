4496ff59539f9a53a2ba7aa577e55be45fe49e10
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-02-02 22:31:05 +0800

    add compoundinterest calculation

--- src/app/portfolio/portfolioController.js
+++ src/app/portfolio/portfolioController.js
@@ -1,42 +1,66 @@
 angularApp.controller('portfolioController', ['$scope', 'projectData', function($scope, projectData) {
   $scope.selectedInterest = '';
   $scope.projectData = projectData.data;
-  $scope.showForm = false;
   $scope.showTable = false;
   $scope.interest;
-  $scope.simpleInterestArray = [];
+  $scope.interestDisplayArr = [];
 
   $scope.onSelectInterest = (e) => {
     $scope.interest = Object.assign({}, projectData.interest);
-    $scope.showForm = $scope.selectedInterest ? true : false;
   }
   $scope.onSubmitForm = () => {
-    calculateSimpleInterest($scope.interest);
+    if ($scope.selectedInterest === 'simple') {
+      makeSimpleInterestArr($scope.interest);
+    } else {
+      makeCompoundInterestArr($scope.interest);
+    }
     $scope.showTable = true;
   }
   $scope.reset = () => {
+    // discard old object by copying new object
     $scope.interest = Object.assign({}, projectData.interest);
+    $scope.showTable = false;
+    $scope.interestDisplayArr = [];
   }
-  function calculateSimpleInterest(interestObj) {
-    console.log(interestObj);
+  
+  function makeSimpleInterestArr(interestObj) {
+    $scope.interestDisplayArr = [];
+    const timeSpan = Number(interestObj.timePeriod);
+    const principal = Number(interestObj.principal);
+    const interestRate = Number(interestObj.rate) / 100;
+    const totalInterest = principal * interestRate;
+
+    for (let i = 0; i <= timeSpan; i++) {
+      $scope.interestDisplayArr.push({
+        period: i,
+        totalAmount: projectData.calculateSimpleAmount(principal, interestRate, i),
+        interestAmount: totalInterest
+      });
+    }
+    console.log('Simple Interest: ', $scope.interestDisplayArr);
+  };
+
+  function makeCompoundInterestArr(interestObj) {
+    $scope.interestDisplayArr = [];
     const timeSpan = Number(interestObj.timePeriod);
     const principal = Number(interestObj.principal);
     const interestRate = Number(interestObj.rate);
-    console.log('timeSpan: ', timeSpan);
-    console.log('principal: ', principal);
-    console.log('InterestRate: ', interestRate);
-
+    const compoundFrequency = Number(interestObj.compoundFrequency);
+    
     for (let i = 0; i <= timeSpan; i++) {
-      $scope.simpleInterestArray.push({
-        amount: calculateSimpleAmount(principal, interestRate, i, ''),
-        period: i
+      var totalAmount = 0;
+      var previousAmount = 0
+      if (i > 0) {
+        totalAmount = projectData.calculateCompoundAmount(principal, interestRate, i, compoundFrequency);
+        previousAmount = $scope.interestDisplayArr[i - 1].totalAmount;
+      }
+      $scope.interestDisplayArr.push({
+        period: i,
+        totalAmount: totalAmount,
+        interestAmount: (totalAmount - previousAmount), 
       });
     }
-    console.log($scope.simpleInterestArray);
+    console.log('Compound Interest: ', $scope.interestDisplayArr);
   }
-
-  function calculateSimpleAmount(principal, interestRate, time, frequency) {
-    return principal + (principal * interestRate * time);
-  } 
 
 }]);;
--- src/app/portfolio/portfolioService.js
+++ src/app/portfolio/portfolioService.js
@@ -14,6 +14,17 @@ angularApp.service('projectData', function($http) {
     principal: '',
     rate: '',
     timePeriod: '',
-    compoundFreq: ''
+    compoundFrequency: ''
   }
+  this.calculateSimpleAmount = function calculateSimpleAmount(principal, interestRate, time) {
+    //  Simple Interest = Principal * annual interest rate * years(or fraction of years):
+    return principal + (principal * interestRate * time);
+  };
+  this.calculateCompoundAmount = function calculateCompoundAmount(principal, interestRate, i, compoundFrequency) {
+    // compund interest = principal * [[1 + (interest/compundFreq)] ^ compoundFreq*years]
+    // var a = (100 * Math.pow((1 + (8/(1*100))), (1*5)));
+    // TO DO: improve rounding - e.g. represent numbers in exponential notation
+    return (principal * Math.pow((1 + (interestRate / (compoundFrequency * 100))), (compoundFrequency * i)));
+  }
+
 });
