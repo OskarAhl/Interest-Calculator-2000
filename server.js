a38d7d00994078293432d6b220cf880272b546bf
Oskar Ahlroth <oskar.ahlroth@gmail.com>
2018-02-03 09:41:45 +0800

    update server endpoint

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ var app = express();
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/src'));
 
-app.get("/", function(req,res) {
+app.get("*", function(req,res) {
   res.render("index.html");
-})
+});
 
 
 app.listen(process.env.PORT || 3000, function(){
-  console.log("listening on port 3000")
+  console.log("Server listening on port 3000");
 });
